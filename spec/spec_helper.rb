3751fbd9afa224cb7ab4ce7ff691eebb627498e1
Ben A Morgan <ben@benmorgan.io>
2014-10-30 23:34:43 -0700

    clean up spec helper configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,17 +19,13 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
-  config.filter_run :focus
   config.run_all_when_everything_filtered = true
-
-  config.disable_monkey_patching!
   config.warnings = true
 
   if config.files_to_run.one?
     config.default_formatter = 'doc'
   end
 
-  config.profile_examples = 10
   config.order = :random
 
   Kernel.srand config.seed
