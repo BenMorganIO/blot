4faeaf6433b2ab2625d3ad89130a4605004db02e
Ben A Morgan <ben@benmorgan.io>
2014-11-08 16:50:54 -0800

    refactor columns to join from an array

--- lib/blot/helpers/grid.rb
+++ lib/blot/helpers/grid.rb
@@ -39,12 +39,15 @@ module Blot
       def columns(width, options={})
         content_tag :table, class: "#{width} columns" do
           content_tag :tr do
-            if options[:sub_columns]
-              "#{yield if block_given?}".html_safe
+            content = if options[:sub_columns]
+              block_given? ? yield : nil
+            elsif !options.empty? || block_given?
+              wrapper(options) { yield if block_given? }
             else
-              "#{wrapper(options) { yield if block_given? } if !options.empty? || block_given? }".html_safe
-            end +
-            "#{content_tag :td, nil, class: 'expander'}".html_safe
+              nil
+            end
+
+            [content, expander].join('').html_safe
           end
         end
       end
@@ -63,6 +66,10 @@ module Blot
       end
 
       private
+
+      def expander
+        content_tag :td, nil, class: 'expander'
+      end
 
       def optional_content(options={}, &block)
         if options[:class] && options[:class].split(' ').include?('center')
