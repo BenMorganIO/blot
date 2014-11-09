7f46271f419f06478cc1ac1672f8ed382fcff193
Ben A Morgan <ben@benmorgan.io>
2014-11-08 18:10:19 -0800

    indent doctype

--- spec/blot/helpers/layout_spec.rb
+++ spec/blot/helpers/layout_spec.rb
@@ -36,7 +36,7 @@ describe Blot::Helpers::Layout do
     example = view.layout(ink: 'ink.css', styles: ['typography.css', 'colors.css'])
 
     expect(example).to eql <<-HTML.compress
-    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+      <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
       <html xmlns="http://www.w3.org/1999/xhtml">
         <head>
           <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
